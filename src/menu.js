ec784942655d1e38184429fe177126147478a7c1
Sebastian Drewes <sebas.drewes@gmail.com>
2021-06-23 00:20:27 -0300

    mejorando display

--- src/menu.js
+++ src/menu.js
@@ -1,22 +1,23 @@
 const menu = () => {
-    const inicioButton = document.querySelector(".inicioButton");
-const menuButton = document.querySelector(".menuButton");
-const contactButton = document.querySelector(".contactButton");
-menuButton.style.cssText = "border-bottom: solid white 2px";
-inicioButton.style.cssText = "border-bottom: hidden";
-contactButton.style.cssText = "border-bottom: hidden";
-    const dynamicZone = document.querySelector(".dynamicZone");
-    while (dynamicZone.firstChild) {
-        dynamicZone.removeChild(dynamicZone.firstChild);
-    }
+  const inicioButton = document.querySelector('.inicioButton');
+  const menuButton = document.querySelector('.menuButton');
+  const contactButton = document.querySelector('.contactButton');
+  menuButton.style.cssText = 'border-bottom: solid white 2px';
+  inicioButton.style.cssText = 'border-bottom: hidden';
+  contactButton.style.cssText = 'border-bottom: hidden';
+  const dynamicZone = document.querySelector('.dynamicZone');
+  while (dynamicZone.firstChild) {
+    dynamicZone.removeChild(dynamicZone.firstChild);
+  }
 
-    const menus = document.createElement("img");
-    menus.classList.add("menu");
-    menus.src = "img/menu.jpeg";
-    menus.addEventListener("click", () => {
-        menus.requestFullscreen();
-    })
+  const menus = document.createElement('img');
+  menus.classList.add('menu');
+  menus.src = 'img/menu.jpeg';
+  menus.style.cssText = 'background-color: black';
+  menus.addEventListener('click', () => {
+    menus.requestFullscreen();
+  });
 
-    dynamicZone.appendChild(menus);
-}
+  dynamicZone.appendChild(menus);
+};
 export { menu };
